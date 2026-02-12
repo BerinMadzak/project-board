e4e53b4f6fc7fad69b1c5463edd93458af1c0af4
BerinMadzak <berinmadzak@gmail.com>
2026-02-12 16:11:30 +0100

    Change project slice name

--- client/src/store/slices/projectSlice.ts
+++ client/src/store/slices/projectSlice.ts
@@ -122,7 +122,7 @@ export const deleteProject = createAsyncThunk(
 );
 
 const projectSlice = createSlice({
-  name: "project",
+  name: "projects",
   initialState: loadInitialState(),
   reducers: {},
   extraReducers: (builder) => {
